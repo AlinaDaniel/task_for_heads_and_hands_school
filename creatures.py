3370f9e0dfdba53dc885b8abd8a8498add13228d
AlinaDaniel <alina.daniel66@icloud.com>
2023-02-24 01:10:00 +0700

    in process.

--- creatures.py
+++ creatures.py
@@ -2,16 +2,62 @@ import random
 
 
 class Creature:
-    N = 0
-    M = 0
+    max_health = 100
 
     def __init__(self, attack, defense, health, damage):
-        self._attack = attack
-        self._defense = defense
-        self._health = health
+        self._attack = Creature.__checked_attack(attack)
+        self._defense = Creature.__checked_defense(defense)
+        self._health = Creature.__checked_health(health)
         self._damage = damage
         self._alive = True
         self._number_of_healing = 0
+
+    @staticmethod
+    def __checked_attack(attack):
+        if not isinstance(attack, int):
+            print("Некорректный тип значения для параметра 'Атака'")
+            raise TypeError
+        if 1 <= attack <= 20:
+            return attack
+        else:
+            print("Некорректное значение для параметра 'Атака'")
+            raise ValueError
+
+    @staticmethod
+    def __checked_defense(defense):
+        if not isinstance(defense, int):
+            print("Некорректный тип значения для параметра 'Защита'")
+            raise TypeError
+        if 1 <= defense <= 20:
+            return defense
+        else:
+            print("Некорректное значение для параметра 'Защита'")
+            raise ValueError
+
+    @staticmethod
+    def __checked_health(health):
+        if not isinstance(health, int):
+            print("Некорректный тип значения для параметра 'Здоровье'")
+            raise TypeError
+        if 1 <= health <= Creature.max_health:
+            return health
+        elif health == 0:
+            print("Попытка создать мертвую сущность")
+            raise ValueError
+        else:
+            print("Некорректное значение для параметра 'Здоровье'")
+            raise ValueError
+
+    @staticmethod
+    def __checked_damage(damage):
+        if not isinstance(damage, range):
+            print("Некорректный тип значения для параметра 'Урон'")
+            raise TypeError
+        if damage[-1] <= Creature.max_health and damage[0] >= 0:
+            return damage
+        else:
+            print("Некорректное значение для параметра 'Урон'")
+            raise ValueError
 
     @property
     def attack(self):
@@ -39,8 +85,14 @@ class Creature:
 
     def healing(self):
         if self._alive and self._number_of_healing < 3:
-            self._health += 0.5 * Creature.N
+            new_health = self.health + 0.5 * Creature.max_health
+            if new_health > Creature.max_health:
+                self._health = Creature.max_health
+            else:
+                self._health = new_health
             self._number_of_healing += 1
+        else:
+            print("Исцеление недоступно")
 
     def hit(self, victim):
         successful_hit = False
@@ -60,7 +112,30 @@ class Gamer(Creature):
     def __init__(self, attack, defense, health, damage):
         super().__init__(attack, defense, health, damage)
 
+    def __str__(self):
+        return f"Сущность Игрок:\nАтака: {self.attack}\nЗащита:" \
+               f"{self.defense}\nЗдоровье:{self.health}\nУрон:{self.damage}"
+
 
 class Monster(Creature):
     def __init__(self, attack, defense, health, damage):
         super().__init__(attack, defense, health, damage)
+
+    def __str__(self):
+        return f"Сущность Монстр:\nАтака: {self.attack}\nЗащита:" \
+               f"{self.defense}\nЗдоровье:{self.health}\nУрон:{self.damage}"
+
+
+def game():
+    g = Gamer(20, 10, 100, range(50))
+    print(g)
+    m = Monster(15, 5, 90, range(40))
+    print(m)
+    print("---")
+    g.hit(m)
+    print(g)
+    print(m)
+    print("---")
+    m.healing()
+    print(g)
+    print(m)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import creatures
 
 
 def print_hi(name):
@@ -11,6 +12,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    creatures.game()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
